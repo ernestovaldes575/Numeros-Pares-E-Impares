8b42484933221cd7931649b20e5af31346fb5809
Ernesto Valdes Lujano <139513381+ernestovaldes575@users.noreply.github.com>
2023-07-19 19:25:52 -0600

    He añadido los archivos básicos de mi ejercicio.

---
+++ Numeros_Pares_E_Impares/app.js
@@ -0,0 +1,14 @@
+//let 1 = 1; Iniciaalizamos el ForLoop
+//i < 20;  Condición
+//i++; = Incremento
+
+for(let i = 2; i < 20; i++){
+    if(i % 2 === 0){
+        console.log(`Par ${i}`)
+        document.write(`Par ${i}`)
+    }else{
+        console.log(`Impar ${i}`);
+        document.write(`Impar ${i}`)
+    }
+    
+}
